48c7545ddf5bdc4ef891f271eebc4eeeae2eca93
Kinga Tokarzewska <kinga@Kingas-MacBook-Air.local>
2018-01-07 13:02:20 +0100

    Add Controler

--- game.js
+++ game.js
@@ -7,13 +7,13 @@ class Component {
 }
 
 class CellComponent extends Component {
-	constructor() {
+	constructor(handleCellClick) {
 		super();
 		this._state = 'unknown';
 		this._element = document.createElement('td');
-		this._element.addEventListener('click', (function() {
-			this.setState('miss');
-		}).bind(this));
+		this._element.addEventListener('click', function() {
+			handleCellClick();
+		});
 		this._refresh();
 	}
 
@@ -28,7 +28,22 @@ class CellComponent extends Component {
 	}
 }
 
-const myCell = new CellComponent();
+class GameController {
+	constructor(cell) {
+		this._cell = cell;
+	}
+	handleCellClick() {
+		this._cell.setState('miss');
+	}
+}
+
+let myController;
+function handleCellClick() {
+	myController.handleCellClick();
+}
+
+const myCell = new CellComponent(handleCellClick);
+myController = new GameController(myCell);
 document
 	.getElementById('cellContainer')
 	.appendChild(myCell.getElement());
